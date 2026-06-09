a631c28015e718b6c720cdd6147568c0727671e7
Олександр Ніценко <niroshi.0@MacBook-Air-Oleksandr-2.local>
2026-06-09 17:39:47 +0200

    Add Lavalink setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "2.2.21"
+    application
 }
 
 group = "org.example"
@@ -11,8 +12,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
     implementation("net.dv8tion:JDA:5.6.1")
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("dev.arbjerg:lavalink-client:3.4.0")
 }
 
 kotlin {
@@ -21,4 +25,13 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.lavalink.dev/releases")
 }
